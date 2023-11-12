318a2409fb20d590bb7cb7e22ea38eab6009af21
Josip Medved <jmedved@jmedved.com>
2023-11-12 13:25:28 -0800

    Refactoring directories

--- Firmware/src/io.c
+++
@@ -1,6 +0,0 @@
-#include <xc.h>
-
-void io_init() {
-    LATC3 = 0;  // start with Active off
-    TRISC3 = 0; // enable Active
-}
--- Firmware/src/timer0.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <xc.h>
-#include <stdbool.h>
-
-void timer0_init(void);
-void timer0_reset(void);
-bool timer0_wasTriggered(void);
-void timer0_waitTick(void);
